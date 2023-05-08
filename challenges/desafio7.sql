20e86575080173d37f0f5210b0f50a1fb915aefd
Felipe <felipe.raindo@gmail.com>
2023-05-08 14:47:09 -0300

    solve req 07

--- challenges/desafio7.sql
+++ challenges/desafio7.sql
@@ -4,7 +4,7 @@ SELECT
   COUNT(follow.followed_artist_id) AS pessoas_seguidoras
 FROM artists
 INNER JOIN albums ON albums.artist_id = artists.id
-INNER JOIN follow ON follow.followed_artist_id = artists.artist_id
+INNER JOIN follow ON follow.followed_artist_id = artists.id
 GROUP BY artists.artist_name, albums.album_name 
 ORDER BY
   pessoas_seguidoras DESC,
