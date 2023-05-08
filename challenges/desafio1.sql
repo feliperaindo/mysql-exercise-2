b1d67d5e766fcf130e78e4067ba93eeb2245c459
Felipe <felipe.raindo@gmail.com>
2023-05-08 16:24:00 -0300

    create file to open PR

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -78,42 +78,42 @@ CREATE TABLE follow (
 INSERT INTO artists (artist_name)
 	VALUES
 		('Beyoncé'),
-    ('Queen'),
-    ('Elis Regina'),
-    ('Baco Exu do Blues'),
-    ('Blind Guardian'),
-    ('Nina Simone');
+        ('Queen'),
+        ('Elis Regina'),
+        ('Baco Exu do Blues'),
+        ('Blind Guardian'),
+        ('Nina Simone');
 
 INSERT INTO plans (type_name, cost)
 	VALUES
 		('gratuito', 0.00),
-    ('familiar', 7.99),
-    ('universitário', 5.99),
-    ('pessoal', 6.99);
+        ('familiar', 7.99),
+        ('universitário', 5.99),
+        ('pessoal', 6.99);
 
 INSERT INTO albums (album_name, release_year, artist_id)
 	VALUES
 		('Renaissance', 2022, 1),
-    ('Jazz', 1978, 2),
-    ('Hot Space', 1982, 2),
-    ('Falso Brilhante', 1998, 3),
-    ('Vento de Maio', 2001, 3),
-    ('QVVJFA?', 2003, 4),
-    ('Somewhere Far Beyond', 2007, 5),
-    ('I Put A Spell On You', 2012, 6);
+        ('Jazz', 1978, 2),
+        ('Hot Space', 1982, 2),
+        ('Falso Brilhante', 1998, 3),
+        ('Vento de Maio', 2001, 3),
+        ('QVVJFA?', 2003, 4),
+        ('Somewhere Far Beyond', 2007, 5),
+        ('I Put A Spell On You', 2012, 6);
 
 INSERT INTO musics (song_name, duration, artist_id, album_id)
 	VALUES
 		("BREAK MY SOUL", 279, 1, 1),
-    ("VIRGO'S GROOVE", 369, 1, 1),
-    ("ALIEN SUPERSTAR", 116, 1, 1),
-    ("Don't Stop Me Now", 203, 2, 2),
-    ("Under Pressure", 152, 2, 3),
-    ("Como Nossos Pais", 105, 3, 4),
-    ("O Medo de Amar é o Medo de Ser Livre", 207, 3, 5),
-    ("Samba em Paris", 267, 4, 6),
-    ("The Bard's Song", 244, 5, 7),
-    ("Feeling Good", 100, 6, 8);
+        ("VIRGO'S GROOVE", 369, 1, 1),
+        ("ALIEN SUPERSTAR", 116, 1, 1),
+        ("Don't Stop Me Now", 203, 2, 2),
+        ("Under Pressure", 152, 2, 3),
+        ("Como Nossos Pais", 105, 3, 4),
+        ("O Medo de Amar é o Medo de Ser Livre", 207, 3, 5),
+        ("Samba em Paris", 267, 4, 6),
+        ("The Bard's Song", 244, 5, 7),
+        ("Feeling Good", 100, 6, 8);
 
 INSERT INTO users_data (user_name, age, subscription_sign_in_date, subscription_plan_id)
 	VALUES
@@ -131,35 +131,35 @@ INSERT INTO users_data (user_name, age, subscription_sign_in_date, subscription_
 INSERT INTO playback_history (reproduction_date, played_by, song_id)
 	VALUES
 		("2022-02-28 10:45:55", 1, 8),
-    ("2020-05-02 05:30:35", 1, 2),
-    ("2020-03-06 11:22:33", 1, 10),
-    ("2022-08-05 08:05:17", 2, 10),
-    ("2020-01-02 07:40:33", 2, 7),
-    ("2020-11-13 16:55:13", 3, 10),
-    ("2020-12-05 18:38:30", 3, 2),
-    ("2021-08-15 17:10:10", 4, 8),
-    ("2022-01-09 01:44:33", 5, 8),
-    ("2020-08-06 15:23:43", 5, 5),
-    ("2017-01-24 00:31:17", 6, 7),
-    ("2017-10-12 12:35:20", 6, 1),
-    ("2011-12-15 22:30:49", 7, 4),
-    ("2012-03-17 14:56:41", 8, 4),
-    ("2022-02-24 21:14:22", 9, 9),
-    ("2015-12-13 08:30:22", 10, 3);
+        ("2020-05-02 05:30:35", 1, 2),
+        ("2020-03-06 11:22:33", 1, 10),
+        ("2022-08-05 08:05:17", 2, 10),
+        ("2020-01-02 07:40:33", 2, 7),
+        ("2020-11-13 16:55:13", 3, 10),
+        ("2020-12-05 18:38:30", 3, 2),
+        ("2021-08-15 17:10:10", 4, 8),
+        ("2022-01-09 01:44:33", 5, 8),
+        ("2020-08-06 15:23:43", 5, 5),
+        ("2017-01-24 00:31:17", 6, 7),
+        ("2017-10-12 12:35:20", 6, 1),
+        ("2011-12-15 22:30:49", 7, 4),
+        ("2012-03-17 14:56:41", 8, 4),
+        ("2022-02-24 21:14:22", 9, 9),
+        ("2015-12-13 08:30:22", 10, 3);
 
 INSERT INTO follow (following_user_id, followed_artist_id)
 	VALUES
 		(1, 1),
-    (1, 2),
-    (1, 3),
-    (2, 1),
-    (2, 3),
-    (3, 2),
-    (4, 4),
-    (5, 5),
-    (5, 6),
-    (6, 6),
-    (6, 1),
-    (7, 6),
-    (9, 3),
-    (10, 2);
+        (1, 2),
+        (1, 3),
+        (2, 1),
+        (2, 3),
+        (3, 2),
+        (4, 4),
+        (5, 5),
+        (5, 6),
+        (6, 6),
+        (6, 1),
+        (7, 6),
+        (9, 3),
+        (10, 2);
