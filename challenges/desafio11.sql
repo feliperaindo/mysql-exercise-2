4693de00cf3ebf7cc7243347e57d471e2a5a3211
Felipe <felipe.raindo@gmail.com>
2023-05-09 20:54:09 -0300

    solve req 11

---
+++ challenges/desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+  albums.album_name AS album,
+  COUNT(fs.song_id) AS favoritadas
+FROM favorite_songs AS fs
+INNER JOIN musics ON fs.song_id = musics.id
+INNER JOIN albums ON albums.id = musics.album_id
+GROUP BY albums.album_name
+ORDER BY favoritadas DESC
+LIMIT 3; 
